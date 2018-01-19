d43398bd6888e17a13e10ebd48526124cf70791e
LuNi <LuNiCoP@orange.fr>
2018-01-19 12:08:10 +0100

    Ogame.Common

---
+++ Utils/StringUtils.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ogame.Common
+{
+    public static class StringUtils
+    {
+        public static string GetStringBetween(string Source, string Start, string End)
+        {
+            // You should check for errors in real-world code, omitted for brevity
+
+            if (Source != string.Empty)
+            {
+                int startIndex = Source.IndexOf(Start) + Start.Length;
+                int endIndex = Source.IndexOf(End, startIndex);
+                if (startIndex < endIndex)
+                    return Source.Substring(startIndex, endIndex - startIndex);
+            }
+            return string.Empty;
+        }
+
+        public static string GetStringBefore(string Source, string Origine, int CaractCount)
+        {
+            int startPos = Source.LastIndexOf(Origine) - CaractCount;
+            if (startPos >= 0)
+                return Source.Substring(startPos, CaractCount);
+            return string.Empty;
+        }
+
+        public static string GetStringAfter(string Source, string Origine, int CaractCount)
+        {
+            int startPos = Source.LastIndexOf(Origine) + Origine.Length;
+            if (startPos >= 0)
+                return Source.Substring(startPos, CaractCount);
+            return string.Empty;
+        }
+
+        public static string GetStringAfter(string Source, string Origine)
+        {
+            int startPos = Source.LastIndexOf(Origine) + Origine.Length;
+            if (startPos >= 0)
+                return Source.Substring(startPos, Source.Length - startPos);
+            return string.Empty;
+        }
+    }
+}
---
+++ Utils/TimerWrapper.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Ogame.Common
+{
+    public class TimerWrapper : IDisposable
+    {
+
+        // Fields
+        private Timer _timer;
+
+
+        // Properties
+        public bool Enabled { get; private set; }
+        public int Interval { get; set; }
+
+
+
+        // Constructor
+        public TimerWrapper(int interval, TimerCallback callback)
+        {
+            Interval = interval;
+            _timer = new Timer(callback, null, Timeout.Infinite, Timeout.Infinite);
+        }
+
+
+        public void Start(bool rightAway = false)
+        {
+            if (Enabled)
+                return;
+
+            Enabled = true;
+            _timer.Change(rightAway ? 0 : Interval, Interval);
+        }
+
+        public void Stop()
+        {
+            if (!Enabled)
+                return;
+
+            Enabled = false;
+            _timer?.Change(Timeout.Infinite, Timeout.Infinite);
+        }
+
+        public void Change(int NewIntervale, bool rightAway = false)
+        {
+            Interval = NewIntervale;
+            if (Enabled) Stop();
+            Start(rightAway);
+        }
+
+        public void Dispose()
+        {
+            _timer?.Dispose();
+            _timer = null;
+        }
+
+    }
+}
